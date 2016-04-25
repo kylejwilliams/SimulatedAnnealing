80d8745f2862c5fd9f530886f06b9e4855794223
Kyle Williams <kjwkc3@mail.umsl.edu>
2016-04-25 00:58:29 -0500

    Pseudocode for the SA algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 
 /**
  * Created by Kyle on 4/24/2016.
+ *
+ * Description: Implement any one version of simulated annealing (SA)-based clustering. Follow the rough framework of
+ * the Maulik et al. paper but use SA instead of the GA. Use 100 randomly generated two dimensional points in
+ * 1.0 <= x, y <= 100.0 and K = 3. Report the final intra- and inter-cluster distances.  I encourage you to search the
+ * Internet for SA pseudocode and code (but write your own code). Report results from more than one run. In your
+ * report, please state your neighbor-generation mechanism, the acceptance criterion, and the annealing schedule.
+ * Make your results reproducible (be careful with your use of the random number generator).
  */
 public class Main {
     private static int numIter = 100;
---
+++ src/SimulatedAnnealing.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Kyle on 4/25/2016.
+ */
+public class SimulatedAnnealing {
+    // First, generate a random solution
+    // Calculate its cost using some cost function you've defined
+    // Repeat:
+        // Generate a random neighboring solution
+        // calculate the new solution's cost
+        // compare them:
+            // if c_new < c_old : move to the new solution
+            // if c_new > c_old : MAYBE move to the new solution
+      // until an acceptable solution is found or you reach some maximum number of iterations
+}
