432c009692f615e0d4541c977049e0f521ec26b8
Kyle Williams <kjwkc3@mail.umsl.edu>
2016-04-27 02:06:53 -0500

    Fixed axes and data displaying out of bounds

--- src/Graph.java
+++ src/Graph.java
@@ -10,8 +10,9 @@ class Graph extends JPanel {
     private static final Stroke GRAPH_STROKE = new BasicStroke(3f);
     private static final int GRAPH_POINT_WIDTH = 12;
     private static final int Y_HATCH_CNT = 10;
+    private static final int X_HATCH_CNT = 10;
     private static final int PREFERRED_WIDTH = 800;
-    private static final int PREFERRED_HEIGHT = 600;
+    private static final int PREFERRED_HEIGHT = 800;
 
     private Data data;
     private List<Point> graphPoints = new ArrayList<>();
@@ -21,34 +22,36 @@ class Graph extends JPanel {
     }
 
     private void createGraphPoints() {
-        double xScale = ((double) getWidth() - 2 * BORDER_GAP) / (data.getDataSize() - 1);
-        double yScale = ((double) getHeight() - 2 * BORDER_GAP) / (MAX_SCORE - 1);
+        double scale = (double)(getWidth() - (2 * BORDER_GAP)) / MAX_SCORE;
+        //double yScale = (double)(getHeight() - (2 * BORDER_GAP)) / MAX_SCORE;
 
         for (int i = 0; i < data.getDataSize(); i++) {
-            int x1 = (int) (i * xScale + BORDER_GAP);
-            int y1 = (int) ((MAX_SCORE - data.getDataPoints()[i].getY() * yScale + BORDER_GAP));
-            graphPoints.add(new Point(x1, y1));
+            int x = (int)(data.getDataPoints()[i].getX() * scale) + BORDER_GAP;
+            int y = (int)(data.getDataPoints()[i].getY() * scale) + BORDER_GAP;
+            graphPoints.add(new Point(x, y));
         }
     }
 
     private void createAxes(Graphics2D g2) {
-        // create x and y axes
-        g2.drawLine(BORDER_GAP, getHeight() - BORDER_GAP, BORDER_GAP, BORDER_GAP);
+        // create y-axis
+        g2.drawLine(BORDER_GAP, BORDER_GAP, BORDER_GAP, getHeight() - BORDER_GAP);
+
+        // create x-axis
         g2.drawLine(BORDER_GAP, getHeight() - BORDER_GAP, getWidth() - BORDER_GAP, getHeight() - BORDER_GAP);
 
-        // create hatch marks for y axis.
+        // create hatch marks
         for (int i = 0; i < Y_HATCH_CNT; i++) {
-            int x0 = BORDER_GAP;
-            int x1 = GRAPH_POINT_WIDTH + BORDER_GAP;
-            int y = getHeight() - (((i + 1) * (getHeight() - BORDER_GAP * 2)) / Y_HATCH_CNT + BORDER_GAP);
-            g2.drawLine(x0, y, x1, y);
-        }
 
-        // and for x axis
-        for (int i = 0; i < data.getDataSize() - 1; i++) {
-            int x = (i + 1) * (getWidth() - BORDER_GAP * 2) / (data.getDataSize() - 1) + BORDER_GAP;
+            // y-axis
+            int y = (i * ((getHeight() - BORDER_GAP) - BORDER_GAP) / Y_HATCH_CNT) + BORDER_GAP;
+            int x0 = BORDER_GAP;
+            int x1 = BORDER_GAP + GRAPH_POINT_WIDTH;
+            g2.drawLine(x0, y, x1, y);
+
+            // x-axis
+            int x = (i * ((getWidth() - BORDER_GAP) - BORDER_GAP) / X_HATCH_CNT) + BORDER_GAP;
             int y0 = getHeight() - BORDER_GAP;
-            int y1 = y0 - GRAPH_POINT_WIDTH;
+            int y1 = getHeight() - BORDER_GAP - GRAPH_POINT_WIDTH;
             g2.drawLine(x, y0, x, y1);
         }
     }
